ae1ff81b33455423d8684c85b187be5c3b252026
Anechka <anechka@anechka-mac.local>
2016-04-25 13:24:59 +0300

    initial commit

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by Anechka on 25.04.16.
+//  Copyright © 2016 Anechka. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    
+    var tweet: Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    func updateUI() {
+        // reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+//        tweetCreatedLabel?.text = nil
+        
+        // load new information from our tweet (if any)
+        if let tweet = self.tweet
+        {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil  {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = NSData(contentsOfURL: profileImageURL) { // blocks main thread!
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+            
+//            let formatter = NSDateFormatter()
+//            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+//                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+//            } else {
+//                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+//            }
+//            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
+        }
+        
+    }
+}
